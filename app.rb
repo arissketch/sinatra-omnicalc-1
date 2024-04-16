d2d179a93a5a3e2f5023c26619ba0e4722138a11
arissketch <60331491+arissketch@users.noreply.github.com>
2024-04-16 22:50:05 +0000

    commit2

--- app.rb
+++ app.rb
@@ -2,8 +2,56 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+
+  erb(:square)
+end
+
+get("/square/new") do
+
+  erb(:square)
+end
+
+get("/square/results") do
+  @inputnum=params.fetch("number").to_f
+  @output=@inputnum.to_f**2
+  erb(:squareresults)
+end
+
+
+get ("/square_root/new") do
+ 
+ erb(:squareroot)
+end
+
+get("/square_root/results") do
+
+  @inputnum=params.fetch("number").to_f
+  @output=@inputnum.to_f**(1/2)
+  erb(:squareroot_results)
+end
+
+get("/random/new") do
+  erb(:random)
+end
+
+get("/random/results") do
+  @min=params.fetch("min").to_f
+  @max=params.fetch("max").to_f
+  @random=rand(@min...@max).to_f
+  erb(:random_results)
+end
+
+
+get ("/payment/new") do
+  erb(:payment)
+end
+
+get("/payment/results") do
+  @n=params.fetch("year").to_i
+  @pv=params.fetch("principal").to_f
+  @r=params.fetch("apr").to_f
+  @numerator = (@r/100/12)*@pv
+  @denominator = (1-(1+@r/100/12)**(-1*@n*12))
+  @payment =@numerator/@denominator
+  erb(:payment_results)
 end
